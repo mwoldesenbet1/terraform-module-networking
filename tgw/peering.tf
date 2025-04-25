7ebb295c2732eab8fe2c33463ce594383f977063
mwoldesenbet1 <mwolde617@gmail.com>
2025-04-25 18:29:56 +0000

    rtb association

--- tgw/peering.tf
+++ tgw/peering.tf
@@ -49,3 +49,43 @@ resource "aws_ec2_transit_gateway_route" "east_to_west" {
 
   depends_on = [aws_ec2_transit_gateway_peering_attachment_accepter.tgw_peering_accepter]
 }
+
+
+
+# Associate VPC attachment with TGW route table in us-west-2
+resource "aws_ec2_transit_gateway_route_table_association" "vpc_west_rt_association" {
+  provider = aws.delegated_account_us-west-2
+  
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_west_attachment.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+
+# Associate VPC attachment with TGW route table in us-east-1
+resource "aws_ec2_transit_gateway_route_table_association" "vpc_east_rt_association" {
+  provider = aws.delegated_account_us-east-1
+  
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_east_attachment.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_east.id
+}
+
+# Associate peering attachment with TGW route table in us-west-2
+resource "aws_ec2_transit_gateway_route_table_association" "peering_west_rt_association" {
+  provider = aws.delegated_account_us-west-2
+  
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+
+  # Ensure the peering is accepted before creating the association
+  depends_on = [aws_ec2_transit_gateway_peering_attachment_accepter.tgw_peering_accepter]
+}
+
+# Associate peering attachment with TGW route table in us-east-1
+resource "aws_ec2_transit_gateway_route_table_association" "peering_east_rt_association" {
+  provider = aws.delegated_account_us-east-1
+  
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_east.id
+
+  # Ensure the peering is accepted before creating the association
+  depends_on = [aws_ec2_transit_gateway_peering_attachment_accepter.tgw_peering_accepter]
+}
