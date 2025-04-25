22477423224cb396fa557bd05a83fc5660352cbc
mwoldesenbet1 <mwolde617@gmail.com>
2025-04-25 17:54:25 +0000

    update rtb

--- vpc/route_table.tf
+++ vpc/route_table.tf
@@ -1,11 +1,20 @@
-# Public Route Tables (without inline routes)
+# Public Route Tables (with inline routes)
 resource "aws_route_table" "public_rt_west" {
   provider = aws.delegated_account_us-west-2
   vpc_id   = aws_vpc.vpc_west.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_west.id
+  }
+
   tags = {
     Name        = "${var.vpc_names["us-west-2"]}-public-rt"
     Environment = "Production"
+  }
+
+  lifecycle {
+    ignore_changes = [route]
   }
 }
 
@@ -13,20 +22,38 @@ resource "aws_route_table" "public_rt_east" {
   provider = aws.delegated_account_us-east-1
   vpc_id   = aws_vpc.vpc_east.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_east.id
+  }
+
   tags = {
     Name        = "${var.vpc_names["us-east-1"]}-public-rt"
     Environment = "NonProduction"
   }
+
+  lifecycle {
+    ignore_changes = [route]
+  }
 }
 
-# Private Route Tables (without inline routes)
+# Private Route Tables (with inline routes)
 resource "aws_route_table" "private_rt_west" {
   provider = aws.delegated_account_us-west-2
   vpc_id   = aws_vpc.vpc_west.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw_west.id
+  }
+
   tags = {
     Name        = "${var.vpc_names["us-west-2"]}-private-rt"
     Environment = "Production"
+  }
+
+  lifecycle {
+    ignore_changes = [route]
   }
 }
 
@@ -34,43 +61,22 @@ resource "aws_route_table" "private_rt_east" {
   provider = aws.delegated_account_us-east-1
   vpc_id   = aws_vpc.vpc_east.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw_east.id
+  }
+
   tags = {
     Name        = "${var.vpc_names["us-east-1"]}-private-rt"
     Environment = "NonProduction"
   }
+
+  lifecycle {
+    ignore_changes = [route]
+  }
 }
 
-# Default routes defined as separate resources
-resource "aws_route" "public_west_to_igw" {
-  provider               = aws.delegated_account_us-west-2
-  route_table_id         = aws_route_table.public_rt_west.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw_west.id
-}
-
-resource "aws_route" "public_east_to_igw" {
-  provider               = aws.delegated_account_us-east-1
-  route_table_id         = aws_route_table.public_rt_east.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw_east.id
-}
-
-resource "aws_route" "private_west_to_nat" {
-  provider               = aws.delegated_account_us-west-2
-  route_table_id         = aws_route_table.private_rt_west.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gw_west.id
-}
-
-resource "aws_route" "private_east_to_nat" {
-  provider               = aws.delegated_account_us-east-1
-  route_table_id         = aws_route_table.private_rt_east.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gw_east.id
-}
-
-# Route Table Associations
-# Assuming first subnet (index 0) is public and second subnet (index 1) is private
+# Route Table Associations remain the same
 resource "aws_route_table_association" "public_rta_west" {
   provider       = aws.delegated_account_us-west-2
   subnet_id      = aws_subnet.subnet_west[0].id
